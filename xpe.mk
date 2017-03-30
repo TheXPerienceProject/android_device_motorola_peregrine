d32dbb1a609826838417017958af0b7e29fd7870
klozz (TeamMEX) <xXx.Reptar.Rawrr.xXx@gmail.com>
2017-03-29 18:16:17 -0600

    xpe love part 2

--- xpe.mk
+++ xpe.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/motorola/peregrine/full_peregrine.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/XPe/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := peregrine
-PRODUCT_NAME := lineage_peregrine
+PRODUCT_NAME := xpe_peregrine
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
