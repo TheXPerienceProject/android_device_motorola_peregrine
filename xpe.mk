5b7fcec6ec2925fc4fa22355b48df104fe102f5d
klozz <klozz707@gmail.com>
2017-09-28 19:57:00 -0400

    peregrine: XPe --> xperience
    Signed-off-by: klozz <klozz707@gmail.com>

--- xpe.mk
+++ xpe.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/motorola/peregrine/full_peregrine.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/XPe/config/common_full_phone.mk)
+$(call inherit-product, vendor/xperience/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := peregrine
 PRODUCT_NAME := xpe_peregrine
